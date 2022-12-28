964298aea5c8ed7bb46d786b90f9c370b86db1dc
heikalb <heikal93@gmail.com>
2022-12-28 15:10:18 -0800

    building

--- post_tweet.py
+++ post_tweet.py
@@ -3,16 +3,29 @@ import datetime
 
 import tweepy
 
-consumer_key = os.getenv("TWITTER_CONSUMER_KEY")
-consumer_secret = os.getenv("TWITTER_CONSUMER_SECRET")
-access_token = os.getenv("TWITTER_ACCESS_TOKEN")
-access_token_secret = os.getenv("TWITTER_ACCESS_TOKEN_SECRET")
-bearer_token = os.getenv("TWITTER_BEARER_TOKEN")
 
-client = tweepy.Client(consumer_key=consumer_key,
-                       consumer_secret=consumer_secret,
-                       access_token=access_token,
-                       access_token_secret=access_token_secret,
-                       bearer_token=bearer_token)
+def get_twitter_client():
+    consumer_key = os.getenv("TWITTER_CONSUMER_KEY")
+    consumer_secret = os.getenv("TWITTER_CONSUMER_SECRET")
+    access_token = os.getenv("TWITTER_ACCESS_TOKEN")
+    access_token_secret = os.getenv("TWITTER_ACCESS_TOKEN_SECRET")
+    bearer_token = os.getenv("TWITTER_BEARER_TOKEN")
 
-response = client.create_tweet(text=f"This is a drone bird. Testing testing... {datetime.datetime.now()}")
+    twitter_client = tweepy.Client(consumer_key=consumer_key,
+                                   consumer_secret=consumer_secret,
+                                   access_token=access_token,
+                                   access_token_secret=access_token_secret,
+                                   bearer_token=bearer_token)
+
+    return twitter_client
+
+
+def get_tweet_text():
+    return f"This is a drone bird. Testing testing... {datetime.datetime.now()}"
+
+
+if __name__ == '__main__':
+    client = get_twitter_client()
+    text_to_tweet = get_tweet_text()
+
+    response = client.create_tweet(text=text_to_tweet)
