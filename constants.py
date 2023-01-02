7e1134ff39a3325aa21c7f1ed1c3ad7f12240831
heikalb <heikal93@gmail.com>
2023-01-02 12:07:58 -0800

    finalize schedule

--- constants.py
+++ constants.py
@@ -1,9 +1,11 @@
 ICBC_DATA_START_YEAR = 2017
 ICBC_DATA_END_YEAR = 2021
+# Limit to intersections with 1 crash/month in five years
 MIN_CRASHES = 60
 
 DEFAULT_AREA = "Vancouver"
 AREA_2_FILE_GLOB = {"Vancouver": "data/vancouver.csv",
                     "Metro Vancouver": "data/metro-van/*.csv"}
 
+# To avoid tweets too long
 MAX_SITE_NAME_LENGHTH = 100
