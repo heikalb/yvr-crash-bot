3d1769259b9466b85ecb9a55c94cfac5c9573bd5
heikalb <mrsandhillcrane@gmail.com>
2023-09-26 18:09:35 -0700

    mention accounts

--- post_death_anniversaries.py
+++ post_death_anniversaries.py
@@ -6,6 +6,8 @@ import datetime
 
 from utils import get_twitter_client
 
+from constants import muni_accounts
+
 MAX_LOC_LEN = 197
 
 
@@ -13,15 +15,17 @@ def get_tweet_text():
     datum = _get_data()
 
     role_verb = f"{datum['ROLE'].lower()}{' was killed' if datum['FATAL_VICTIM_COUNT'] == '1' else 's were killed'}"
+    muni = string.capwords(datum['CITY'])
+    display_muni = muni_accounts.get(muni, muni)
 
     if datum['LOCATION_NAME']:
         location_preposition = "at" if "&" in datum['LOCATION_NAME'] else "on"
         location = datum['LOCATION_NAME'].split("#")[0].strip()
         # To keep within Twitter char limit
         location = f"{datum['LOCATION_NAME'][:MAX_LOC_LEN - 3]}..." if len(location) > MAX_LOC_LEN else location
-        full_location = f"{location_preposition} {string.capwords(location)}, {string.capwords(datum['CITY'])}"
+        full_location = f"{location_preposition} {string.capwords(location)}, {display_muni}"
     else:
-        full_location = f"in {string.capwords(datum['CITY'])}"
+        full_location = f"in {display_muni}"
 
     ret_text = (f"In this month in {datum['ACCIDENT_YEAR']}\n"
                 f"{datum['FATAL_VICTIM_COUNT']} {role_verb} in a car crash {full_location}")
